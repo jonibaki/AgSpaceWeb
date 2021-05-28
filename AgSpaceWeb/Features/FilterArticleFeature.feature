f7637322a85d9bc9e9a60d084c5dd5cbe57e2e6e
Jonibaki <joni.786@hotmail.co.uk>
2021-05-28 01:30:10 +0100

    Add FilterArticle feature and steps files

---
+++ AgSpaceWeb/Features/FilterArticleFeature.feature
@@ -0,0 +1,28 @@
+﻿Feature: Filter Articles
+	As a User
+	I want to find out  certain articles depending on different filter values
+
+@smoke
+Scenario: Check Filters Funtionality 
+	Given I browse AgSpace website
+
+	When I click What's New from Nav bar
+	Then I navigate What's New page
+	
+	When I toggle on only product
+	Then I filtered product related articles
+
+	When I toggle on only market
+	Then I filtered market related articles
+
+	When I toogle on only communication
+	Then I filtered communication related articles
+
+	When I toogle on only date range
+	Then I filtered date range related article
+
+	When I toogle on multiple categories
+	Then I filtered multiple categories related articles
+
+
+
---
+++ AgSpaceWeb/Steps/FilterArticlesSteps.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AgSpaceWeb.Steps
+{
+    [Binding]
+    public class FilterArticlesSteps
+    {
+        [Given(@"I browse AgSpace website")]
+        public void GivenIBrowseAgSpaceWebsite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click What's New from Nav bar")]
+        public void WhenIClickWhatSNewFromNavBar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I toggle on only product")]
+        public void WhenIToggleOnOnlyProduct()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I toggle on only market")]
+        public void WhenIToggleOnOnlyMarket()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I toogle on only communication")]
+        public void WhenIToogleOnOnlyCommunication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I toogle on only date range")]
+        public void WhenIToogleOnOnlyDateRange()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I toogle on multiple categories")]
+        public void WhenIToogleOnMultipleCategories()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I navigate What's New page")]
+        public void ThenINavigateWhatSNewPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I filtered product related articles")]
+        public void ThenIFilteredProductRelatedArticles()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I filtered market related articles")]
+        public void ThenIFilteredMarketRelatedArticles()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I filtered communication related articles")]
+        public void ThenIFilteredCommunicationRelatedArticles()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I filtered date range related article")]
+        public void ThenIFilteredDateRangeRelatedArticle()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I filtered multiple categories related articles")]
+        public void ThenIFilteredMultipleCategoriesRelatedArticles()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
